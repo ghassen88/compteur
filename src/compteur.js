2eeba0cfaafa1ab14f97adcd9abc29c4e9de40e1
ghassen <Vous@exemple.com>
2019-02-12 14:09:26 +0100

    first commit

---
+++ src/compteur.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+const Compte=()=>{
+    return (
+        <div className="compteur">
+        <div className="copmte1">
+        <div>00</div>
+        <div>:</div>
+        <div>30</div>
+        <div>:</div>
+        <div>00</div>
+        </div>
+
+
+
+        </div>
+
+    );
+}
+export default Compte;
---
+++ src/timing.js
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import './App.css';
+
+
+
+
+
+
+const Timing=()=>{
+    return(
+  <div className="time">
+<div className="time1">
+  <div className="time11">Hour</div>
+  <div className="time22">Minute</div>
+  <div className="time33">Second</div>
+  </div>
+  </div>
+
+
+
+    );
+}
+export default Timing;
